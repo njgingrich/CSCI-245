fd3700f87402d092c16e0eaef515a4c79d11d5e1
njgingrich <njgingrich@gmail.com>
2015-09-23 20:23:45 +0000

    kegg reactions parsing now working

--- 9_23/kegg.pl
+++ 9_23/kegg.pl
@@ -6,16 +6,44 @@ use strict;
 while (<>) {
     if (/(?<reaction>R\d+)\s*:\s*(?<map_id>\d+):\s*(?<in>[^<]*)(?<arrow><?=>?)\s*(?<out>.*)/) {
         my ($reaction, $map, $in, $arrow, $out) = ($+{reaction}, $+{map_id}, $+{in}, $+{arrow}, $+{out});
+        my @substrates = getList($in);
+        my @products = getList($out);
+
         print $_;
         print "\n";
         print "reaction is ",  $reaction, "\n";
         print "map is ",       $map, "\n";
         print "direction is ", $arrow, "\n";
-        print "substrate is ", $in, "\n";
-        print "product is ",   $out, "\n";
-        if ($in =~ /\+/) {
-            
-        }
-    }
 
+        if ($arrow ne "<=") {
+            printArray("substrate", @substrates);
+            printArray("product", @products);
+        }
+
+        if ($arrow eq "<=" || $arrow eq "<=>") { # switch products and substrates
+            printArray("substrate", @products);
+            printArray("product", @substrates);
+        }
+        print "\n";
+    }
+}
+
+sub getList {
+    my ($str) = @_;
+    my @array;
+    if ($str !~ /\+/) {
+        return ($str); # list of value
+    }
+    while ($str =~ /(?<i>C\w+)(?:\s*\+\s*)?/g) {
+        my $i = $+{i};
+        push @array, $i;
+    }
+    return @array;
+}
+
+sub printArray {
+    my ($name, @array) = @_;
+    foreach my $i (@array) {
+        print "$name is $i\n";
+    }
 }
